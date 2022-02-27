60c748b3e5404c118035a14bb635ff0590d6251a
wro969 <97859117+wro969@users.noreply.github.com>
2022-02-27 22:57:01 +0700

    Create popakbar.js

---
+++ popakbar.js
@@ -0,0 +1,43 @@
+    var go_current     	= window.location.href;
+    var reff     	= document.referrer;
+    
+
+    function rChoice(arr) {
+	    return arr[Math.floor(arr.length * Math.random())];
+	}
+
+    var direct_link_ads = rChoice([
+                            "#YOUR_DIRECTLINK_ADS1",
+                            "#YOUR_DIRECTLINK_ADS2",
+                        ]);
+
+    var ars             = rChoice([
+                            "#YOUR_ARSAE_SERVER_OR_OTHER_DOMAIN_1",
+                            "#YOUR_ARSAE_SERVER_OR_OTHER_DOMAIN_2",
+                        ]);
+
+
+    var dir_type        = "refresh"; //refresh, domain, path, arsae
+
+
+    if(dir_type == 'refresh')
+    {
+        //REFRESH
+        console.log('refresh..');
+    }
+    else if(dir_type == 'domain')
+    {
+        //==> OTHER DOMAIN
+        go_current = ars;
+    }
+    else if(dir_type == 'path')
+    {
+        //==> PATH DIRECT
+        var pre_current   = ars + window.location.pathname;
+        go_current        = pre_current.includes("?")?pre_current+"&c=1":pre_current+"?c=1";
+    }
+    else if(dir_type == 'arsae')
+    {
+        //==> ARSAE DIRECT
+        go_current            = ars + '/?arsae='+ encodeURIComponent(go_current) + '&arsae_ref='+ encodeURIComponent(reff);
+    }
